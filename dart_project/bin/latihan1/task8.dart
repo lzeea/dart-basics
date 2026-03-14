b4c5b816ac774d5ab5b7028d1a8f2a35101d4b81
lzeea <limifrohatinjepara@gmail.com>
2026-03-15 00:38:45 +0700

    update task8

--- dart_project/bin/latihan1/task8.dart
+++ dart_project/bin/latihan1/task8.dart
@@ -1,17 +1,17 @@
 import 'dart:io';
 
 void main() {
-  stdout.write("Masukkan nilai A: ");
+  stdout.write("Masukkan Nilai A: ");
   int a = int.parse(stdin.readLineSync()!);
-  
-  stdout.write("Masukkan nilai B: ");
+
+  stdout.write("Masukkan Nilai B: ");
   int b = int.parse(stdin.readLineSync()!);
-  
+
   print("Sebelum ditukar: A = $a, B = $b");
-  
+
   int temp = a;
   a = b;
   b = temp;
-  
+
   print("Setelah ditukar: A = $a, B = $b");
 }
