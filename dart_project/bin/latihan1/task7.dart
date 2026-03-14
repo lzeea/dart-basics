12ed92ee019463aaac0b597e67b13805a5a5a169
lzeea <limifrohatinjepara@gmail.com>
2026-03-15 00:35:08 +0700

    update task7

--- dart_project/bin/latihan1/task7.dart
+++ dart_project/bin/latihan1/task7.dart
@@ -1,15 +1,15 @@
 import 'dart:io';
 
 void main() {
-  stdout.write("Masukkan bilangan pertama: ");
+  stdout.write("Masukkan bilangan Pertama: ");
   int bil1 = int.parse(stdin.readLineSync()!);
-  
-  stdout.write("Masukkan bilangan kedua: ");
+
+  stdout.write("Masukkan bilangan Kedua: ");
   int bil2 = int.parse(stdin.readLineSync()!);
-  
+
   int hasilBagi = bil1 ~/ bil2; // Menggunakan integer division
   int sisa = bil1 % bil2; // Menggunakan modulo
-  
+
   print("Hasil bagi: $hasilBagi");
   print("Sisa pembagian: $sisa");
 }
