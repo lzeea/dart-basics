e71983073c5ce918dd5048d48363d06a0f2970f9
lzeea <limifrohatinjepara@gmail.com>
2026-03-15 00:44:19 +0700

    update task11

--- dart_project/bin/latihan1/task11.dart
+++ dart_project/bin/latihan1/task11.dart
@@ -3,13 +3,14 @@ import 'dart:io';
 void main() {
   stdout.write("Masukkan total jumlah tagihan: Rp");
   double totalTagihan = double.parse(stdin.readLineSync()!);
-  
+
   stdout.write("Masukkan jumlah orang: ");
   int jumlahOrang = int.parse(stdin.readLineSync()!);
-  
+
   if (jumlahOrang > 0) {
     double pembagian = totalTagihan / jumlahOrang;
-    print("Masing-masing orang harus membayar: Rp${pembagian.toStringAsFixed(2)}");
+    print(
+        "Masing-masing orang harus membayar: Rp${pembagian.toStringAsFixed(2)}");
   } else {
     print("Jumlah orang harus lebih dari 0.");
   }
