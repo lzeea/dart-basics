918735bafb4f2e18c53d0ff9f4f1cd26e70366b5
lzeea <limifrohatinjepara@gmail.com>
2026-03-15 00:12:51 +0700

    update task2

--- dart_project/bin/latihan1/task2.dart
+++ dart_project/bin/latihan1/task2.dart
@@ -1,9 +1,9 @@
 import 'dart:io';
 
 void main() {
-  stdout.write("Masukkan nama Anda: ");
+  stdout.write("Masukkan nama Kamu: ");
   String? nama = stdin.readLineSync();
-  
+
   print('Hello, saya "$nama"');
   print("Hello, saya '$nama'");
 }
