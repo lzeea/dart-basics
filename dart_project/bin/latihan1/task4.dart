b561dab420e46455d14b9cc85ed3b68e3d59998e
lzeea <limifrohatinjepara@gmail.com>
2026-03-15 00:22:21 +0700

    update task4

--- dart_project/bin/latihan1/task4.dart
+++ dart_project/bin/latihan1/task4.dart
@@ -1,15 +1,15 @@
 import 'dart:io';
 
 void main() {
-  stdout.write("Masukkan nilai p (pokok pinjaman): ");
+  stdout.write("Masukkan Nilai p (pokok pinjaman): ");
   double p = double.parse(stdin.readLineSync()!);
-  
-  stdout.write("Masukkan nilai t (waktu dalam tahun): ");
+
+  stdout.write("Masukkan Nilai t (waktu dalam tahun): ");
   double t = double.parse(stdin.readLineSync()!);
-  
-  stdout.write("Masukkan nilai r (suku bunga dalam persen): ");
+
+  stdout.write("Masukkan Nilai r (suku bunga dalam persen): ");
   double r = double.parse(stdin.readLineSync()!);
-  
+
   double bungaSederhana = (p * t * r) / 100;
-  print("Bunga sederhana adalah: $bungaSederhana");
+  print("Bunga Sederhana adalah: $bungaSederhana");
 }
