3add612ed4a41a4306e0d1f424cca0ead0dc1f1b
lzeea <limifrohatinjepara@gmail.com>
2026-03-15 00:31:04 +0700

    udate task5

--- dart_project/bin/latihan1/task5.dart
+++ dart_project/bin/latihan1/task5.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 void main() {
   stdout.write("Masukkan sebuah angka: ");
   double angka = double.parse(stdin.readLineSync()!);
-  
+
   double persegi = angka * angka;
   print("Persegi dari angka $angka adalah: $persegi");
 }
